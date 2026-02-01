cae8bd54e56a3a4573746c76127fb2c87aa3689f
Amar Singh <amar298976@gmail.com>
2026-02-01 11:44:41 +0530

    Adding Search feature

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
-import { fetchPhotos, fetchVideos } from './api/mediaApi'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
 
-  async function getPhoto(){
-    const data = await fetchPhotos('cat')
-    console.log(data.results);
-    
-  }
-  async function getVideo(){
-    const data = await fetchVideos('cat')
-    console.log(data.videos);
-    
-  }
+
 
   return (
     <div className='h-screen w-full bg-gray-950 text-white'>
-      <h1>Hello</h1>
-      <button className='bg-blue-500 text-white px-4 py-2 rounded-full m-5' onClick={getPhoto}>Get Photo</button>
-       <button className='bg-blue-500 text-white px-3 py-2 rounded-full m-5' onClick={getVideo}>Get video</button>
+     <SearchBar/>
     </div>
   )
 }
---
+++ project/src/components/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import {useDispatch} from 'react-redux'
+import {setQuery} from '../redux/features/searchSlice'
+
+const SearchBar = () => {
+  const [text, setText] = useState("");
+
+  const dispatch = useDispatch()
+
+  const sumbitHandler = (e) => {
+    e.preventDefault();
+
+    dispatch(setQuery(text))
+    // console.log(text);
+    setText('')
+  };
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          sumbitHandler(e);
+        }}
+        className="flex gap-5 bg-gray-900 p-10"
+      >
+        <input
+          type="text"
+          className="w-full border-2 px-4 py-2 text-xl rounded outline-none"
+          required
+          value={text}
+          onChange={(e)=>{
+            setText(e.target.value)
+          }}
+          placeholder="Search...."
+        />
+        <button className="bg-blue-500 text-white active:scale-95 cursor-pointer border-2 px-4 py-2 text-xl rounded-full outline-none">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
